c041e1e94e2571d6962e07187f7fcfd74e377f83
ivangajicgg <ivangajicgg@gmail.com>
2020-06-25 14:56:07 +0200

    First commit

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const searchUser = document.getElementById('searchUser');
 searchUser.addEventListener('keyup', (e) => {
   // Get input text
   const userText = e.target.value;
+  console.log(userText)
 
   if(userText !== ''){
    // Make http call
--- github.js
+++ github.js
@@ -1,9 +1,11 @@
 class Github {
   constructor() {
     this.client_id = 'd9308aacf8b204d361fd';
+    this.users_count = 5;
     this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
     this.repos_count = 5;
     this.repos_sort = 'created: asc';
+    this.q = 'Marija';
   }
 
   async getUser(user) {
@@ -11,8 +13,12 @@ class Github {
 
     const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const searchUsers = await fetch(`https://api.github.com/search/users?q=${user}&per_page=${this.users_count}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
     const repos = await repoResponse.json();
+    const search = await searchUsers.json();
+    console.log(search)
 
     return {
       profile,
